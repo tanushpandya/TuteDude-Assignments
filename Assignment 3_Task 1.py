ce1d48504b6638bbff85e9a3cf4552971f42788d
tanushpandya <pandyatanush@gmail.com>
2025-08-11 23:26:32 +0530

    Add files via upload

---
+++ Assignment 3_Task 1.py
@@ -0,0 +1,9 @@
+def factorial(n):
+        if (n==1 or n==0):
+            return 1
+        return n * factorial(n-1)
+    
+
+n=int(input('Enter a number:'))
+
+print(f'Factorial of {n} is {factorial(n)}')
---
+++ Assignment 3_Task 2.py
@@ -0,0 +1,4 @@
+import math
+n=int(input('Enter a number:'))
+
+print(f'Square Root: {math.sqrt(n)}\nLogarithm: {math.log(n)}\nSine: {math.sin(n)}')
